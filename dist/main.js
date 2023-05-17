d16bd8595ce249229bc3dd9712dcfb1d4f367a1e
Cess <cessmbuguar@gmail.com>
2023-05-17 15:18:07 +0300

    functionality test page

---
+++ dist/main.js
@@ -0,0 +1,65 @@
+
+// var asset = new  GLTFUtils.GLTFAsset()
+
+
+
+// let gltf = require('gltf-js-utils');
+//let imgField = document.getElementById('img-field');
+
+// const { Moment} = Mome
+// let moment = require('moment');
+// let date = new moment();
+// console.log(date.format('ddd'));
+
+
+
+
+
+
+//harmony import */ var gltf_js_utils__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(gltf_js_utils__WEBPACK_IMPORTED_MODULE_0__);
+//let gltfJsUtils = require("gltf-js-utils");
+// var asset1 = new gltf.GLTFAsset();
+// console.log(asset1);
+// var asset = new gltf_js_utils__WEBPACK_IMPORTED_MODULE_0__.GLTFAsset();
+// console.log("here now!!");
+// console.log(asset);
+
+// import {
+//   GLTFAsset, Scene, Node, Material, Texture, Mesh, Vertex, WrappingMode
+// } from "gltf-js-utils";
+//  const asset = new GLTFAsset();
+//  console.log(asset);
+// const scene = new Scene();
+// asset.addScene(scene);
+
+// const node = new Node();
+// node.setTranslation(x, y, z);
+// node.setRotationRadians(x, y, z);
+// node.setScale(x, y, z);
+// scene.addNode(node);
+
+// const material = new Material();
+// // Supported texture types: HTMLImageElement | HTMLCanvasElement | ArrayBuffer (PNG) | Data URL (PNG)
+// const texture = new Texture(image);
+// texture.wrapS = WrappingMode.CLAMP_TO_EDGE;
+// texture.wrapT = WrappingMode.REPEAT;
+// material.pbrMetallicRoughness.baseColorTexture = texture;
+ 
+// const mesh = new Mesh();
+// mesh.material = [material];
+// node.mesh = mesh;
+ 
+// const v1 = new Vertex();
+// v1.x = 1;
+// v1.y = 1;
+// v1.z = 1;
+// v1.u = 0;
+// v1.v = 0;
+// const v2 = new Vertex();
+// // ...
+ 
+// const faceColor = undefined;
+// const faceMaterialIndex = 0;
+// mesh.addFace(v1, v2, v3, faceColor, faceMaterialIndex);
+// mesh.addFace(v4, v5, v6, faceColor, faceMaterialIndex);
+// console.log(node);
