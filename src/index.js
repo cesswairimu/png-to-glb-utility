d31cc68a3c12bf78f8c8f8fe084ee743255e79fc
Cess <cessmbuguar@gmail.com>
2024-01-18 07:37:21 +0300

    tailwind styling

---
+++ src/index.js
@@ -0,0 +1,14 @@
+
+// const checkbox = document.getElementById("checkboxRoughness");
+// const grayBox = document.getElementById("gray-box");
+
+// checkbox.addEventListener("change", () => {
+//   if (checkbox.checked) {
+//     grayBox.classList.remove("hidden");
+//   } else {
+//     grayBox.classList.add("hidden");
+//   }
+// });
+
+// $('#checkboxRoughness').on
+
